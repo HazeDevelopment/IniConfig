0c65efab3d85af2101456114846f8190ca66c0b7
Juriën Hamaker <jurienhamaker@Hotmail.com>
2015-09-02 23:30:18 +0200

    first working piece

--- src/IniConfig.php
+++ src/IniConfig.php
@@ -1,82 +1,279 @@
 <?php
 namespace HazeDevelopment;
 
-use \WriteIniFile\WriteIniFile;
-
+/**
+ * Class WriteIniFile
+ *
+ */
 class IniConfig
 {
-	public $ini;
-	public static $path;
-	public static $config;
+    /**
+     * @var string $path_to_ini_file
+     * @var array $data_ini_file
+     */
+    protected $path_to_ini_file;
+    protected $data_ini_file;
 
-	public function __construct()
-	{
-		$this->ini = new WriteIniFile(base_path(config('iniconfig.file_path')));
-		dd($this->ini);
-	}
+    /**
+     * Constructor.
+     *
+     * @param string $ini_file
+     */
+    public function __construct()
+    {
+        $this->path_to_ini_file = base_path(config('iniconfig.file_path'));
+        if (file_exists($this->path_to_ini_file) === true) {
+            $this->data_ini_file = @parse_ini_file($this->path_to_ini_file, true);
+        } else {
+            $this->data_ini_file = [];
+        }
+        if (false === $this->data_ini_file) {
+            throw new \Exception(sprintf('Unable to parse file ini : %s', $this->path_to_ini_file));
+        }
+    }
 
-	public function __get($name)
-	{
-		if($this->config[$name])
+    /**
+     * method to get value already loaded
+     *
+     * @param string $name
+     */
+    public function get($name)
+    {
+    	if(strpos($name, '.') !== false)
+    	{
+    		$splitted = explode(".", $name);
+    		$top = $splitted[0];
+    		$sub = $splitted[1];
+
+    		if(!array_key_exists($top, $this->data_ini_file))
+    		{
+    			return false;
+    		}
+
+    		if(!array_key_exists($sub, $this->data_ini_file[$top]))
+    		{
+    			return false;
+    		}
+
+    		return $this->data_ini_file[$top][$sub];
+    	}
+
+    	if(!array_key_exists($name, $this->data_ini_file))
 		{
-			return $this->config[$name];	
+			return false;
 		}
-		else
+
+    	return $this->data_ini_file[$name];
+    }
+
+    /**
+     * method to set value in the ini file.
+     *
+     * @param string $name
+     * @param string $value
+     */
+    public function set($name, $value)
+    {
+		if(strpos($name, '.') !== false)
+    	{
+    		$splitted = explode(".", $name);
+    		$top = $splitted[0];
+    		$sub = $splitted[1];
+
+    		if(!array_key_exists($top, $this->data_ini_file))
+    		{
+    			$this->data_ini_file[$top] = [];
+    			$this->data_ini_file[$top][$sub] = $value;
+    		}
+    		else
+    		{
+    			$this->data_ini_file[$top][$sub] = $value;
+    		}
+    	}
+    	else
+    	{
+    		$this->data_ini_file[$name] = $value;
+    	}
+    	
+    	return $this;
+    }
+
+
+    /**
+     * method to check if ini file contains value
+     * @param string $name
+     */
+    public function has($name)
+    {
+    	if(strpos($name, '.') !== false)
+    	{
+    		$splitted = explode(".", $name);
+    		$top = $splitted[0];
+    		$sub = $splitted[1];
+
+    		if(!array_key_exists($top, $this->data_ini_file))
+    		{
+    			return false;
+    		}
+    		else
+    		{
+    			if(!array_key_exists($sub, $this->data_ini_file[$top]))
+	    		{
+	    			return false;
+	    		}
+    		}
+
+    		return true;
+    	}
+
+    	if(!array_key_exists($name, $this->data_ini_file))
 		{
-			return False;
+			return false;
 		}
-	}
 
-	public function __set($name, $value)
-	{
-		self::$config[$name] = $value;
-	}
+    	return true;
+    }
 
-	public static function get($name)
-	{
-		if(self::$config[$name])
-		{
-			return self::$config[$name];	
-		}
-		else
-		{
-			return False;
-		}
-	}
+    /**
+     * method to get all values
+     * @param string $name
+     */
+    public function all()
+    {
+    	return $this->data_ini_file;
+    }
 
-	public static function set($name, $value)
-	{
-		self::$config[$name] = $value;
-	}
+    /**
+     * method for change value in the ini file.
+     *
+     * @param array $new_value
+     */
+    public function update(array $new_value)
+    {
+        $this->data_ini_file = array_replace_recursive($this->data_ini_file, $new_value);
+    }
 
-	public static function all()
-	{
-		return self::$config;
-	}
+    /**
+     * method for create ini file.
+     *
+     * @param array $new_ini_file
+     */
+    public function create(array $new_ini_file)
+    {
+        $this->data_ini_file = $new_ini_file;
+    }
 
-	private static function save()
-	{
-		$count = 0;
-	    $res = array();
-	    foreach(self::$config as $key => $val)
-	    {
-	        if(is_array($val))
-	        {
-	            $res[] = ($count != 0 ? "\r\n" : "").'['.$key.']';
-	            foreach($val as $skey => $sval) $res[] = $skey." = '".$sval."'";
-	        }
-	        else $res[] = $key.' = '.$val;
-	        $count ++;
-	    } 
+    /**
+     * method for erase ini file.
+     *
+     */
+    public function erase()
+    {
+        $this->data_ini_file = [];
+    }
 
-	    if (!$handle = fopen(self::$path, 'w')) { 
-	        return false; 
-	    }
+    /**
+     * method for add new value in the ini file.
+     *
+     * @param array $add_new_value
+     */
+     public function add(array $add_new_value)
+     {
+         $this->data_ini_file = array_merge_recursive($this->data_ini_file, $add_new_value);
+     }
 
-	    $success = fwrite($handle, implode("\r\n", $res));
-	    fclose($handle); 
+     /**
+     * method for remove some values in the ini file.
+     *
+     * @param array $add_new_value
+     */
+     public function rm(array $rm_value)
+     {
+         $this->data_ini_file = self::arrayDiffRecursive($this->data_ini_file, $rm_value);
+     }
 
-	    return $success; 
-	}
-   
+    /**
+     * method for write data in the ini file.
+     *
+     * @return bool true for a succes
+     */
+    public function write()
+    {
+        $data_array = $this->data_ini_file;
+        $file_content = null;
+        foreach ($data_array as $key_1 => $groupe) {
+            $file_content .= "\n[" . $key_1 . "]\n";
+            foreach ($groupe as $key_2 => $value_2) {
+                if (is_array($value_2)) {
+                    foreach ($value_2 as $key_3 => $value_3) {
+                        $file_content .= $key_2 . '[' . $key_3 . '] = ' . self::encode($value_3) . "\n";
+                    }
+                } else {
+                    $file_content .= $key_2 . ' = ' . self::encode($value_2) . "\n";
+                }
+            }
+        }
+        $file_content = preg_replace('#^\n#', '', $file_content);
+        $result = @file_put_contents($this->path_to_ini_file, $file_content);
+        if (false === $result) {
+            throw new \Exception(sprintf('Unable to write in the file ini : %s', $this->path_to_ini_file));
+        }
+        return ($result !== false) ? true : false;
+    }
+
+    /**
+     * method for encode type for ini file.
+     *
+     * @param mixed $value
+     * @return string
+     */
+    private static function encode($value)
+    {
+        if ($value == '1' || $value === true) {
+            return 1;
+        }
+        if ($value == '' || $value == '0' || $value === false) {
+            return 0;
+        }
+        if (is_numeric($value)) {
+            $value = $value * 1;
+            if (is_int($value)) {
+                return (int) $value;
+            }
+            if (is_float($value)) {
+                return (float) $value;
+            }
+        }
+        return '"' . $value . '"';
+    }
+
+    /**
+     * Computes the difference of 2 arrays recursively
+     * source : http://php.net/manual/en/function.array-diff.php#91756
+     *
+     * @param array $array1
+     * @param array $array2
+     * @return array
+     */
+    private static function arrayDiffRecursive(array $array1, array $array2)
+    {
+        $finalArray = [];
+        foreach ($array1 as $mKey => $mValue) {
+            if (array_key_exists($mKey, $array2)) {
+                if (is_array($mValue)) {
+                    $arrayDiffRecursive = self::arrayDiffRecursive($mValue, $array2[$mKey]);
+                    if (count($arrayDiffRecursive)) {
+                        $finalArray[$mKey] = $arrayDiffRecursive;
+                    }
+                } else {
+                    if ($mValue != $array2[$mKey]) {
+                        $finalArray[$mKey] = $mValue;
+                    }
+                }
+            } else {
+                $finalArray[$mKey] = $mValue;
+            }
+        }
+        return $finalArray;
+    }
 }
